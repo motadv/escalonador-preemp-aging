2a10ab8dec926ab1b90246267104152f2937f1c1
motadv <102678270+motadv@users.noreply.github.com>
2022-10-28 17:46:40 -0300

    Escalonador pronto

--- Escalonador.py
+++ Escalonador.py
@@ -1,4 +1,8 @@
+from glob import glob
+
+
 class Process:
+    
     def __init__(self, enterTime, name, priority, burstList, ioList) -> None:
         self.enterTime = enterTime
         self.name = name
@@ -6,78 +10,140 @@ class Process:
         self.burstList = burstList
         self.ioList = ioList
 
-    def debug_print(self):
-        print(f"{self.enterTime} {self.name} {self.priority} {self.burstList} {self.ioList}")
+    def readProcess(text):
+        info = text.split()
+        enterTime = int(info[0])
+        name = info[1]
+        priority = int(info[2])
+        burstList = [int(x) for x in info[3::2]]
+        ioList = [int(x) for x in info[4::2]]
+
+        return Process(enterTime, name, priority, burstList, ioList)
+
+def enterReady(p: Process):    
+    readyQueue.append(p)
+    interruption()
+
+def interruption():
+    global exec
+    
+    if exec != None:
+        readyQueue.append(exec)
+        exec = None
+        print('> Interrupt <')
+
+def enterWait(p: Process):
+    waitQueue.append(p)
+    
+def waitIOs():
+    global waitQueue
+    
+    for p in waitQueue:
+        p.ioList[0] -= 1
+        if p.ioList[0] == 0:
+            p.ioList.remove(p.ioList[0])
+            enterReady(p)
+            waitQueue.remove(p)
+
+def aging():
+    global readyQueue
+    
+    #decrease pririty
+    for p in readyQueue:
+        if p.priority > 0:
+            p.priority -= 1
+            
+    interruption()
+
+def getNextProcess()-> Process:
+    global readyQueue
+    
+    readyQueue.sort(key=lambda x: (x.priority, x.enterTime))
+    if readyQueue: return readyQueue.pop(0)
+    else: return None
 
 
-def readProcess(text)->Process:
-    info = text.split()
-    enterTime = int(info[0])
-    name = info[1]
-    priority = int(info[2])
-    burstList = [int(x) for x in info[3::2]]
-    ioList = [int(x) for x in info[4::2]]
+def execute():  
+    global exec, quantum, readyQueue
+    
+    if exec == None:
+        #enter new process in exec
+        exec = getNextProcess()
+        quantum = 0
+    
+    #execute current
+    if exec != None:
+        exec.burstList[0] -= 1
+        quantum += 1
+        
+        if exec.burstList[0] == 0:
+            del exec.burstList[0]
+            if exec.ioList:
+                enterWait(exec)
+                exec = None
+            else:
+                processEndedCallback(exec)
+                exec = getNextProcess()
+                
+                quantum = 0
+                
+        if quantum > 5:
+            quantum = 0
+            aging()
+            exec = getNextProcess()
+        
 
-    return Process(enterTime, name, priority, burstList, ioList)
+def processEndedCallback(p: Process):
+    print(f'O processo: {p.name} terminou!')
 
 
-timeLine = []
-with open("processos.in","r") as inputFile:
-    for line in inputFile:
-        timeLine.append(readProcess(line))
-
-timeLine.sort(key = lambda x: x.enterTime)
-
-for x in timeLine:
-    x.debug_print()
 time = 0
 quantum = 0
 readyQueue = []
 waitQueue = []
 exec = None
 
-def enterReady(p: Process):
-    readyQueue.append(p)
-    interruption()
+timeLine = []
+with open("processos.in","r") as inputFile:
+    for line in inputFile:
+        timeLine.append(Process.readProcess(line))
 
-def interruption():
-    if exec != None:
-        readyQueue.append(exec)
-        exec = None
-
-def waitIOs():
-    for p in waitQueue:
-        p.ioList[0] -= 1
-        if p.ioList[0] == 0:
-            del p.ioList[0]
+timeLine.sort(key = lambda x: x.enterTime)
+for p in timeLine:
+        if p.enterTime == time:
             enterReady(p)
-            waitQueue.remove(p)
-
-def execute():  
-    if exec == None:
-        #enter new process in exec
-        readyQueue.sort(key=lambda x: x.priority)
-        exec = readyQueue.pop(0)
-        quantum = 0
+            timeLine.remove(p)
     
     
-
-
-
-        
 #Processing Loop base:
 #While there are processes in the system
-while(len(timeLine) != 0 and len(readyQueue) != 0 and len(waitQueue) != 0 and exec == None):
+print("Escalonador com Prioridade, Aging e Preemp:\n")
+
+while(exec or readyQueue or waitQueue):
     
     #Entra processos novos na lista de pronto
-    for item in timeLine:
-        if item.enterTime == time:
-            enterReady(item)
-            timeLine.remove(item)
-        else:
-            break
+    for p in timeLine:
+        if p.enterTime == time:
+            enterReady(p)
+            timeLine.remove(p)
 
     #Resolve o IO da fila de espera
     waitIOs()
     execute()
+    
+    
+    print(f'Estado Atual:\n')
+    print(f'Tempo: {time} | Aging: {quantum}')
+    print(f'Executando: ', end='')
+    if exec != None: print(f'{exec.name} | Prioridade: {exec.priority} | Tempo restante: {exec.burstList[0]}', end='')
+    print(f'\nFila de Pronto:')
+    for p in readyQueue:
+        print(f'   {p.name} | Prioridade: {p.priority} | Tempo restante: {p.burstList[0]}')
+    print(f'Fila de Espera:')
+    for p in waitQueue:
+        print(f'   {p.name} | Prioridade: {p.priority} | Tempo restante: {p.ioList[0]}')
+    
+    
+    print("---------------------------------------------")
+    
     time += 1
